3a3ec161a185efe80becc4aca44ceaa99cb5dddd
XQH <108980460+yggdrasummer@users.noreply.github.com>
2023-08-22 23:40:17 +0800

    Update chat.go
    加了点注释

--- back-end/chat.go
+++ back-end/chat.go
@@ -11,7 +11,7 @@ import (
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
-		return true //允许任何来源的WebSocket连接
+		return true //允许任何来源的WebSocket连接，这个主要是为了防止同源策略
 	},
 }
 
@@ -24,8 +24,8 @@ func main() {
 	r.LoadHTMLGlob("chat.html")
 
 	r.GET("/:username", func(c *gin.Context) {
-		username := c.Param("username") //从请求的url里解析username出来传到前端去
-		c.HTML(http.StatusOK, "chat.html", gin.H{"username": username})
+		username := c.Param("username") //从请求的url里解析username出来
+		c.HTML(http.StatusOK, "chat.html", gin.H{"username": username})  //把username传到前端去
 	})
 	//前端来建立websocket连接
 	r.GET("/ws", handleWebSocket)
@@ -34,12 +34,12 @@ func main() {
 }
 
 func handleWebSocket(c *gin.Context) {
-	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
-	if err != nil {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)   //获得了websocket.Conn对象conn
+	if err != nil {   //如果出错的话
 		fmt.Println("Error upgrading to WebSocket:", err)
 		return
 	}
-	defer conn.Close()
+	defer conn.Close()  //在整个函数执行完之后执行
 
 	username := c.Query("username") //就用username做客户端标识
 
